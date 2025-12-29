c1b8afbcbb29caeb672d447ecc444e94c7621290
Bereket Getachew <bakiget78@gmail.com>
2025-12-29 11:40:32 +0300

    Refactor authentication module and update Prisma schema to include User, Session, Account, and Verification models. Enhance ESLint configuration and update package dependencies for improved functionality and compatibility.

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -1,34 +1,32 @@
-// @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+// eslint.config.mjs
+import { js } from '@eslint/js';
 import globals from 'globals';
-import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-  {
-    ignores: ['eslint.config.mjs'],
-  },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      sourceType: 'commonjs',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
+export default {
+  root: true,
+  languageOptions: {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      tsconfigRootDir: process.cwd(),
+      project: './tsconfig.json',
+      sourceType: 'module',
+    },
+    globals: {
+      ...globals.node,
+      ...globals.jest,
     },
   },
-  {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
-    },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-floating-promises': 'warn',
+    '@typescript-eslint/no-unsafe-argument': 'warn',
   },
-);
+  ignorePatterns: ['eslint.config.mjs'],
+};
